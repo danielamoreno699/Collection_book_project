961d1a91f9a3e97d383ec6fcad90831e0b232d25
Juan Sanchez <juansan1993@hotmail.com>
2023-03-20 15:20:47 -0500

    Remove uncessary code

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
+const ul = document.getElementById('ul-list');
+const form = document.getElementById('form');
+const inputAuthor = document.getElementById('author');
+const inputBook = document.getElementById('book');
 
-
-
-const ul = document.getElementById("ul-list");
-const form = document.getElementById("form");
-const inputAuthor = document.getElementById("author")
-const inputBook = document.getElementById("book")
-
-
-const bookDetails = JSON.parse(localStorage.getItem("newBookObject")) || null
-
-
-//
+const bookDetails = JSON.parse(localStorage.getItem('newBookObject')) || null;
 
 // setting initial object
-const bookInformation ={
-    author: '',
-    book: ''
-
-}
+const bookInformation = {
+  author: '',
+  book: '',
+};
 
 //Creating a dynamic LI template
-
-function addBook(){
-    bookDetails.forEach((bookInfo) => {
-        const newBook = document.createElement('li')
-        newBook.innerHTML = `
+function addBook() {
+  bookDetails.forEach((bookInfo) => {
+    const newBook = document.createElement('li');
+    newBook.innerHTML = `
         <p class="book">${bookInfo.book}</p>
         <p clas="author">${bookInfo.author}</p>
         <button type="button">Remove</button>
         <hr>
-        `
-        ul.appendChild(newBook)
-    })
+        `;
+    ul.appendChild(newBook);
+  });
 }
 
-addBook()
-
+addBook();
 
 //form function
-form.addEventListener("submit", (e) =>{
-    e.preventDefault()
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    if( inputAuthor === '' || inputBook === ''){
-        return
-    }
-    const newBook = { author: inputAuthor.value, book: inputBook.value}
+  if (inputAuthor === '' || inputBook === '') {
+    return;
+  }
+  const newBook = { author: inputAuthor.value, book: inputBook.value };
 
-    bookDetails.push(newBook)
-    console.log(bookDetails)
+  bookDetails.push(newBook);
+  console.log(bookDetails);
 
-    localStorage.setItem("newBookObject", JSON.stringify(bookDetails))
-
-})
+  localStorage.setItem('newBookObject', JSON.stringify(bookDetails));
+});
