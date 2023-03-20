2fe0f76e7b99a7e63b3f1ab9f2257424619f4021
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-20 14:50:02 -0500

    Add array, and object for property creation and functions

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log('Hello World');
+
+
+
+const ul = document.getElementById("ul-list");
+const form = document.getElementById("form");
+const inputAuthor = document.getElementById("author")
+const inputBook = document.getElementById("book")
+
+
+const bookDetails = JSON.parse(localStorage.getItem("newBookObject")) || null
+
+
+//
+
+// setting initial object
+const bookInformation ={
+    author: '',
+    book: ''
+
+}
+
+//Creating a dynamic LI template
+
+function addBook(){
+    bookDetails.forEach((bookInfo) => {
+        const newBook = document.createElement('li')
+        newBook.innerHTML = `
+        <p class="book">${bookInfo.book}</p>
+        <p clas="author">${bookInfo.author}</p>
+        <button type="button">Remove</button>
+        <hr>
+        `
+        ul.appendChild(newBook)
+    })
+}
+
+addBook()
+
+
+//form function
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    if( inputAuthor === '' || inputBook === ''){
+        return
+    }
+    const newBook = { author: inputAuthor.value, book: inputBook.value}
+
+    bookDetails.push(newBook)
+    console.log(bookDetails)
+
+    localStorage.setItem("newBookObject", JSON.stringify(bookDetails))
+
+})
