a971d18aa8bf1102dc2014e0a3f26859e50f32c2
Juan Sanchez <juansan1993@hotmail.com>
2023-03-21 16:13:17 -0500

    Update JS with classes and methods

--- index.js
+++ index.js
@@ -1,44 +1,110 @@
-/* eslint-disable no-unused-vars */
-
-const ul = document.getElementById('ul-list');
-const form = document.getElementById('form');
-const inputAuthor = document.getElementById('author');
-const inputBook = document.getElementById('book');
-
-const bookDetails = JSON.parse(localStorage.getItem('newBooksAdded')) || [];
-
-function addBook() {
-  ul.innerHTML = '';
-  for (let i = bookDetails.length - 1; i >= 0; i -= 1) {
-    const bookInfo = bookDetails[i];
-    const newBook = document.createElement('li');
-    newBook.innerHTML = `
-      <p class="book">${bookInfo.book}</p>
-      <p class="author">${bookInfo.author}</p>
-      <button type="button" onclick="removeItem(${i})">Remove</button>
-      <hr>
-    `;
-    ul.insertBefore(newBook, ul.firstChild);
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 }
 
-function removeItem(index) {
-  bookDetails.splice(index, 1);
-  addBook();
-  localStorage.setItem('newBooksAdded', JSON.stringify(bookDetails));
+class UI {
+  addBookToTheList(book) {
+    const ul = document.getElementById('ul-list');
+
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+    <span class="title">${book.title}</span> by 
+    <span class="author">${book.author}</span>
+    <button class="delete">Remove</button>
+  `;
+    ul.insertBefore(li, ul.firstChild);
+  }
+
+  removeBook(target) {
+    if (target.className === 'delete') {
+      target.parentElement.remove();
+    }
+    console.log('click1');
+  }
+
+  clearFieldsInputs() {
+    const author = document.getElementById('author');
+    const title = document.getElementById('book');
+    author.value = '';
+    title.value = '';
+  }
 }
 
-// remove item
-removeItem();
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function (book) {
+      const ui = new UI();
+      ui.addBookToTheList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBookFromStore(index) {
+    const books = Store.getBooks();
+
+    books.splice(index, 1);
+    //console.log('click2')
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  Store.displayBooks();
+});
+
+// Event listening to form
+const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (inputAuthor.value === '' || inputBook.value === '') {
+  const author = document.getElementById('author').value;
+  const title = document.getElementById('book').value;
+
+  //instantiate book
+  const book = new Book(author, title);
+
+  //instantiate UI
+  const ui = new UI();
+
+  if (author === '' || title === '') {
     return;
   }
-  const newBook = { author: inputAuthor.value, book: inputBook.value };
-  bookDetails.unshift(newBook);
-  localStorage.setItem('NewBookAdded', JSON.stringify(bookDetails));
-  addBook();
-  inputAuthor.value = '';
-  inputBook.value = '';
+
+  ui.addBookToTheList(book);
+  Store.addBook(book);
+  ui.clearFieldsInputs();
+  event.preventDefault();
+});
+
+//event listening for delete book
+document.getElementById('ul-list').addEventListener('click', function (e) {
+  const ui = new UI();
+  ui.removeBook(e.target);
+  const bookTitle = e.target.parentElement.querySelector('.title').textContent;
+  const bookAuthor =
+    e.target.parentElement.querySelector('.author').textContent;
+  const books = Store.getBooks();
+  const index = books.findIndex(
+    (book) => book.title === bookTitle && book.author === bookAuthor
+  );
+  Store.removeBookFromStore(index);
+  e.preventDefault();
 });
